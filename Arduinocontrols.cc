685bb6c130efce622ed72854dc99031e3199670c
Evan Xiang <109013894+evankxiang@users.noreply.github.com>
2022-08-05 17:51:25 -0400

    Create Arduinocontrols.cc

---
+++ Arduinocontrols.cc
@@ -0,0 +1,34 @@
+
+#include <LiquidCrystal.h>
+const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+#define echoPin 2 
+#define trigPin 3 
+long duration; 
+int distance; 
+
+void setup() {
+  //set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600); 
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); 
+  Serial.println("with Arduino UNO R3");
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  lcd.print("Distance: ");
+  lcd.print(distance);
+  lcd.println(" cm");
+}
